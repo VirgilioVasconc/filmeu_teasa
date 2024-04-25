1bc6154c3e8c8e3a1adf865033124a44e850bfef
VirgilioVasconc <160045985+VirgilioVasconc@users.noreply.github.com>
2024-04-25 12:57:27 +0200

    Update header.php

--- header.php
+++ header.php
@@ -2,13 +2,20 @@
 		<a rel="home" title="Home - TEASA - The European Archive of Short Animation" href="/"><img src="img/TEASA_logo.png" alt="TEASA - The European Archive of Short Animation" /></a>
 
 		<a title="FilmEU_RIT" href="https://www.filmeu.eu/" target="_blank"><img src="img/FilmEU_RIT_logo_H_Pink.png" alt="FilmEU_RIT" /></a>
-<!--
+		<br />
 		<nav>
-			<ul>
-				<li><a href="#">Home</a></li>
-				<li><a href="#">Add film</a></li>
-				<li><a href="#">Login</a></li>
-			</ul>
+			<div class="nav-links">
+				<a class="nav-link" href="/">Home</a>
+				<a class="nav-link" href="https://www.filmeu.eu/research/pilot-projects/the-european-archive-of-short-animation">About</a>
+				<a class="nav-link" href="/files/TEASA_Summit_Report_April_2024.pdf">Our Report</a>
+				<a class="nav-link" >Login</a>
+			</div>
+			<div class="hamburger">&#9776;</div>
 		</nav>
--->
+		<script>
+			document.querySelector('.hamburger').addEventListener('click', function() {
+			  const navLinks = document.querySelector('.nav-links');
+			  navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+			});
+		</script>
 </header>
